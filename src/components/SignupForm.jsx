7042824f7380ed24bf2a4d4547e0df5bf8c13415
TIDYMOUSE <eulerandfriends@gmail.com>
2024-08-31 16:08:53 +0530

    feat: dashboard negative version

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,70 +1,95 @@
-import React ,{useState} from 'react'
-import {createUserWithEmailAndPassword , onAuthStateChanged} from 'firebase/auth'
-import {firebaseAuth ,db} from "../utils/firebase-config"
-import {useNavigate} from 'react-router-dom'
+import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { firebaseAuth, db } from "../utils/firebase-config";
+import { useNavigate } from "react-router-dom";
 import { MdOutlineEmail } from "react-icons/md";
 import { CiLock } from "react-icons/ci";
-import Cookies from 'js-cookie'
-import {addDoc,collection} from 'firebase/firestore'
+import Cookies from "js-cookie";
+import { addDoc, collection } from "firebase/firestore";
 
 const SignupForm = () => {
-  const[form,setform] = useState({
-    email:"",
-    password:"",
-  })
+  const [form, setform] = useState({
+    email: "",
+    password: "",
+  });
   const navigate = useNavigate();
-  const[error,seterror]= useState(null);
+  const [error, seterror] = useState(null);
 
-  const handleSubmit = async ()=>{
-    
+  const handleSubmit = async () => {
     try {
-      const{email,password} = form
-      const userCredential = await createUserWithEmailAndPassword(firebaseAuth,email,password)
+      const { email, password } = form;
+      const userCredential = await createUserWithEmailAndPassword(
+        firebaseAuth,
+        email,
+        password
+      );
       const token = await userCredential.user.getIdToken();
-      Cookies.set('jwtToken', token, { expires: 7, secure: true, sameSite: 'strict' });
+      Cookies.set("jwtToken", token, {
+        expires: 7,
+        secure: true,
+        sameSite: "strict",
+      });
       await addDoc(collection(db, "users"), {
         uid: userCredential.user.uid,
         email: email,
         isAdmin: false,
       });
-
     } catch (error) {
-      console.log(error.code)
-      seterror(error.code)
+      console.log(error.code);
+      seterror(error.code);
     }
-
   };
 
-  onAuthStateChanged(firebaseAuth,(currentUser)=>{
- 
-    if(currentUser){
-      
-       navigate('/dashboard')};
-  })
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) {
+      navigate("/dashboard");
+    }
+  });
 
   return (
     <div>
-    <div className='flex flex-col my-8 gap-8'>
+      <div className="flex flex-col my-8 gap-8">
+        <div className="border-black border-2 rounded-full px-6 py-3 flex items-center">
+          <MdOutlineEmail className="text-xl mr-4 text-Red" />
+          <input
+            className="text-black bg-transparent focus:outline-none "
+            type="email"
+            placeholder="Email ID"
+            name="email"
+            id="email"
+            value={form.email}
+            onChange={(e) => {
+              setform({ ...form, [e.target.name]: e.target.value });
+            }}
+          />
+        </div>
 
-      <div className='border-white border-2 rounded-full px-6 py-3 flex items-center'>
-
-        <MdOutlineEmail className='text-xl mr-4 text-Red' />
-        <input className='text-slate-300 bg-transparent  focus:outline-none ' 
-        type="email" placeholder='Email ID' name="email" id="email" value={form.email} onChange={(e)=>{setform({...form, [e.target.name]:e.target.value})}}/>
+        <div className="border-black border-2 rounded-full px-6 py-3 flex items-center">
+          <CiLock className="mr-4 text-Red text-2xl" />
+          <input
+            className="text-black bg-transparent  focus:outline-none "
+            type="password"
+            placeholder="Password"
+            name="password"
+            id="password"
+            value={form.password}
+            onChange={(e) => {
+              setform({ ...form, [e.target.name]: e.target.value });
+            }}
+          />
+        </div>
+        <button
+          onClick={handleSubmit}
+          className="bg-Black text-White rounded-full p-2 bg-secondary/90 hover:bg-secondary text-white font-bold transition-all duration-200"
+        >
+          Sign Up
+        </button>
       </div>
-
-      <div className='border-white border-2 rounded-full px-6 py-3 flex items-center'>
-
-        <CiLock className='mr-4 text-Red text-2xl'/>
-        <input className='text-slate-300 bg-transparent  focus:outline-none ' 
-        type="password" placeholder='Password' name="password" id="password" value={form.password} onChange={(e)=>{setform({...form, [e.target.name]:e.target.value})}}/>
-      </div>
-      <button onClick={handleSubmit} className='bg-Black text-White rounded-full p-2 hover:bg-black font-bold transition-colors duration-200'>Sign Up</button>
     </div>
+  );
+};
 
-    
-    </div>
-  )
-}
-
-export default SignupForm
+export default SignupForm;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,23 +1,119 @@
-import { onAuthStateChanged } from 'firebase/auth';
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { firebaseAuth } from '../utils/firebase-config';
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { firebaseAuth } from "../utils/firebase-config";
+import { Pie } from "react-chartjs-2";
+import data from "../constants/dash.json";
+import { annualCO2inMilTonnes } from "../utils/emission";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Dashboard = () => {
-    const navigate=useNavigate();
+  const navigate = useNavigate();
+  const coalTypes = Object.keys(data);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(firebaseAuth, (currentUser) => {
-          if (!currentUser) {
-            navigate("/")
-          }
-        });
-    
-        return () => unsubscribe();
-      }, [navigate]);
+  // Calculate emissions for each coal type
+  const emissionData = coalTypes.map((coalType) => {
+    const {
+      yearlyProduction,
+      exclusionFactor,
+      conversionFactorTJ_kt,
+      co2EmissionFactor_kg_CO2_TJ,
+    } = data[coalType];
+
+    // Calculate CO2 emission for the current coal type
+    return annualCO2inMilTonnes(
+      yearlyProduction,
+      coalType,
+      exclusionFactor,
+      conversionFactorTJ_kt,
+      co2EmissionFactor_kg_CO2_TJ
+    );
+  });
+
+  const chartData = {
+    labels: coalTypes,
+    datasets: [
+      {
+        label: "CO2 Emission in Million Tonnes",
+        data: emissionData,
+        backgroundColor: [
+          "#FF6384",
+          "#36A2EB",
+          "#FFCE56",
+          "#4BC0C0",
+          "#9966FF",
+        ],
+        hoverBackgroundColor: [
+          "#FF6384",
+          "#36A2EB",
+          "#FFCE56",
+          "#4BC0C0",
+          "#9966FF",
+        ],
+      },
+    ],
+  };
+
+  let emission = 0;
+  Object.keys(data).forEach((coalType) => {
+    const {
+      yearlyProduction,
+      exclusionFactor,
+      conversionFactorTJ_kt,
+      co2EmissionFactor_kg_CO2_TJ,
+    } = data[coalType];
+
+    emission += annualCO2inMilTonnes(
+      yearlyProduction,
+      coalType,
+      exclusionFactor,
+      conversionFactorTJ_kt,
+      co2EmissionFactor_kg_CO2_TJ
+    );
+  });
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(firebaseAuth, (currentUser) => {
+      if (!currentUser) {
+        navigate("/");
+      }
+    });
+
+    return () => unsubscribe();
+  }, [navigate]);
+
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="flex flex-col gap-4 w-full bg-background min-h-screen p-5">
+      <section className="w-full flex gap-4 items-start">
+        <div className="flex flex-col  w-1/4 space-y-4 bg-primary p-5  rounded-md shadow-md">
+          <h2 className="text-white text-4xl font-semibold">CO2 EMISSION</h2>
+          <h2 className="text-white text-xl">
+            <span className="text-4xl font-semibold text-accent underline">
+              {parseFloat(emission).toFixed(3)}
+            </span>{" "}
+            million Tonnes
+          </h2>
+        </div>
 
-export default Dashboard
+        <div className="flex justify-center items-center w-full bg-white shadow-md rounded-md h-1/3">
+          <Pie data={chartData} />
+        </div>
+      </section>
+
+      <section className="w-full flex flex-col bg-white p-5 shadow-md rounded-md mt-5">
+        <h2 className="text-2xl font-semibold mb-3">Manage Admins</h2>
+
+        <div className="flex w-full flex-col p-3 gap-2 rounded-md bg-background">
+          <div className="p-2 bg-white rounded-md ">XDD</div>
+          <div className="p-2 bg-white rounded-md ">XDD</div>
+          <div className="p-2 bg-white rounded-md ">XDD</div>
+          <div className="p-2 bg-white rounded-md ">XDD</div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Dashboard;
